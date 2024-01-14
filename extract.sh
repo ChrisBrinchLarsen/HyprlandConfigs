65bceb054632167539bd2dcc9662dedf948eaec3
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2024-01-14 21:46:38 +0100

    fixed pathings

--- extract.sh
+++ extract.sh
@@ -4,6 +4,6 @@ cp ~/.config/alacritty/alacritty.toml ~/HyprlandConfigs
 cp ~/.config/waybar/config ~/HyprlandConfigs
 cp ~/.config/waybar/style.css ~/HyprlandConfigs
 
-cp /boot/loader/entries/arch.conf ~/HyprlandConfigs/nvidia
+cp /boot/loader/entries/*linux.conf ~/HyprlandConfigs/nvidia
 cp /etc/mkinitcpio.conf ~/HyprlandConfigs/nvidia
 cp /etc/modprobe.d/nvidia.conf ~/HyprlandConfigs/nvidia
