8b61d1b1e2990911eb941a9e305bd22805f1435b
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2024-01-22 10:47:35 +0100

    Daddy first push

--- extract.sh
+++ extract.sh
@@ -4,6 +4,11 @@ cp ~/.config/alacritty/alacritty.toml ~/HyprlandConfigs
 cp ~/.config/waybar/config ~/HyprlandConfigs
 cp ~/.config/waybar/style.css ~/HyprlandConfigs
 
+cp ~/.scripts/setwp.sh ~/HyprlandConfigs/scripts
+cp ~/.scripts/wprename.sh ~/HyprlandConfigs/scripts
+cp ~/.bashrc ~/HyprlandConfigs
+cp ~/changes ~/HyprlandConfigs
+
 cp /boot/loader/entries/*linux.conf ~/HyprlandConfigs/nvidia
 cp /etc/mkinitcpio.conf ~/HyprlandConfigs/nvidia
 cp /etc/modprobe.d/nvidia.conf ~/HyprlandConfigs/nvidia
