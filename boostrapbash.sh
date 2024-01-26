e32d3da9282aa89f69679758df101273ef41dd3d
Zupreme <chrisbrinchlarsen@gmail.com>
2024-01-26 12:27:05 +0100

    bashrc

---
+++ boostrapbash.sh
@@ -0,0 +1,8 @@
+mkdir ~/.bash
+mkdir ~/.bash_prompt
+mkdir ~/.bash_utils
+cp ~/HyprlandConfigs/.bash/.bash_aliases ~/.bash/
+cp ~/HyprlandConfigs/.bash/.bash_prompt ~/.bash/
+cp ~/HyprlandConfigs/.bash/.bash_utils ~/.bash/
+cp ~/HyprlandConfigs/.bashrc ~/
+source ~/.bashrc
